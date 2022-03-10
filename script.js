4e321a9f088a5bc8dfd4358c77851a1cf5c3f665
bsord <brandon.sorgdrager@gmail.com>
2022-03-10 11:52:56 -0600

    Add player and handle movement

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.addEventListener('load', function(){
   class InputHandler {
     constructor(){
       this.keys = [];
+      
       window.addEventListener('keydown', e => {
         if((  e.key === 'ArrowDown' ||
               e.key === 'ArrowUp' ||
@@ -28,7 +29,62 @@ window.addEventListener('load', function(){
   }
 
   class Player {
+    constructor(gameWidth, gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById('playerImage');
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+      this.vy = 0;
+      this.weight = 1;
+    }
 
+    draw(context){
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+
+    update(input){
+      
+      // horizontal movement
+      if (input.keys.indexOf('ArrowRight') > -1){
+        this.speed = 5;
+      } else if (input.keys.indexOf('ArrowLeft') > -1) {
+        this.speed = -5;
+      }  else {
+        this.speed = 0;
+      }
+      this.x+= this.speed;
+      if(this.x < 0){
+        this.x = 0;
+      } else if (this.x > this.gameWidth - this.width) {
+        this.x = this.gameWidth - this.width;
+      }
+      
+      // vertical movement
+      if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+        this.vy -= 32;
+      }
+      this.y += this.vy;
+      if(!this.onGround()){
+        this.vy += this.weight;
+        this.frameY = 1;
+      } else {
+        this.vy=0;
+        this.frameY = 0;
+      }
+      if(this.y > this.gameHeight - this.height){this.y=this.gameHeight-this.height}
+    }
+    
+    onGround(){
+      return this.y >= this.gameHeight - this.height;
+    }
   }
 
   class Background {
@@ -48,9 +104,13 @@ window.addEventListener('load', function(){
   }
 
   const input = new InputHandler();
-
-  function animate() {
-
-  }
+  const player = new Player(canvas.width, canvas.height);
   
+  function animate() {
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    player.draw(ctx);
+    player.update(input)
+    requestAnimationFrame(animate);
+  }
+  animate();
 })
